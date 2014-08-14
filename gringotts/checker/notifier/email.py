46bcb33ba6a22096467e99a8326d4a7fe074820b
Guangyu Suo <guangyu@unitedstack.com>
2014-08-14 15:02:14 +0800

    send owed email to support@unitedstack.com
    Change-Id: I6893ac5da95766dd2d53167856b5d9a2aae083fd

--- gringotts/checker/notifier/email.py
+++ gringotts/checker/notifier/email.py
@@ -14,6 +14,7 @@ class EmailNotifier(notifier.Notifier):
 
     @staticmethod
     def notify_has_owed(context, account, contact, orders):
+        # Notify user
         account_name = contact['email'].split('@')[0]
         resources = 'resources' if len(orders) > 1 else 'resource'
         subject = u"[UnitedStack] 您好, %s, 您有%s个资源已经欠费" % (account_name, len(orders))
@@ -34,9 +35,28 @@ class EmailNotifier(notifier.Notifier):
         notify = gring_notifier.get_notifier(service='checker')
         notify.info(context, 'uos.account.owed', payload)
 
+        # Notify us
+        subject = u"[UnitedStack] 用户[%s]已欠费，有%s个欠费资源" % (contact['email'], len(orders))
+        payload = {
+            'actions': {
+                'email': {
+                    'template': 'account_has_owed',
+                    'context': {
+                        'orders': orders,
+                        'reserved_days': account['reserved_days']
+                    },
+                    'subject': subject,
+                    'from': 'noreply@unitedstack.com',
+                    'to': 'support@unitedstack.com'
+                }
+            }
+        }
+        notify = gring_notifier.get_notifier(service='checker')
+        notify.info(context, 'uos.account.owed', payload)
 
     @staticmethod
     def notify_before_owed(context, account, contact, price_per_day, days_to_owe):
+        # Notify user
         subject = u"[UnitedStack] 您好, %s, 您的账户余额不足, 请及时充值" % contact['email'].split('@')[0]
         payload = {
             'actions': {
@@ -50,6 +70,26 @@ class EmailNotifier(notifier.Notifier):
                     'subject': subject,
                     'from': 'noreply@unitedstack.com',
                     'to': contact['email']
+                }
+            }
+        }
+        notify = gring_notifier.get_notifier(service='checker')
+        notify.info(context, 'uos.account.will_owed', payload)
+
+        # Notify us
+        subject = u"[UnitedStack] 用户[%s]的账户余额即将不足" % contact['email']
+        payload = {
+            'actions': {
+                'email': {
+                    'template': 'account_will_owe',
+                    'context': {
+                        'price_per_day': price_per_day,
+                        'balance': str(account['balance']),
+                        'days_to_owe': days_to_owe
+                    },
+                    'subject': subject,
+                    'from': 'noreply@unitedstack.com',
+                    'to': 'support@unitedstack.com'
                 }
             }
         }
