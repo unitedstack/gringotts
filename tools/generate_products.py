15b4e153483fa46a902b6e447dd9b2b199d94872
Guangyu Suo <guangyu@unitedstack.com>
2015-12-15 08:40:54 +0000

    Optimize the generate_products.py script
    Change-Id: I93e5806ddf9481f76a4475cac4aa7b71598c63e4
    Type: Other
    Jira: BILL-137

--- tools/generate_products.py
+++ tools/generate_products.py
@@ -4,6 +4,7 @@ import os
 import sys
 import requests
 
+from decimal import Decimal, ROUND_HALF_UP
 from gringotts.client import client as gring_client
 from novaclient.v1_1 import client as nova_client
 
@@ -29,6 +30,12 @@ parser.add_argument('--recreate_flavor',
                     metavar='BOOL', type=bool, default=False,
                     help='Recreate flavor or not')
 args = parser.parse_args()
+
+
+def quantize(value):
+    if isinstance(value, Decimal):
+        return value.quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP)
+    return Decimal(str(value)).quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP)
 
 
 def force_v3_api(url):
@@ -98,8 +105,15 @@ def clear_flavors():
         nc.flavors.delete(flavor.id)
 
 
+def _generate_seg_price(base_seg_price, times):
+    result = []
+    for price in base_seg_price:
+        result.append([price[0], str(times * quantize(price[1]))])
+    return result
+
+
 def make_seg_price(hourly_price=None, monthly_price=None,
-                   yearly_price=None):
+                   yearly_price=None, auto_generated=True):
     seg = {}
     if hourly_price:
         seg.update({"price": {
@@ -107,20 +121,26 @@ def make_seg_price(hourly_price=None, monthly_price=None,
             "type": "segmented",
             "segmented": hourly_price[1]
         }})
-    if monthly_price:
+    if hourly_price and (monthly_price or auto_generated):
+        if not monthly_price:
+            monthly_price = (hourly_price[0],
+                             _generate_seg_price(hourly_price[1], 720))
         seg.update({"monthly_price": {
             "base_price": monthly_price[0],
             "type": "segmented",
             "segmented": monthly_price[1]
         }})
-    if yearly_price:
+    if monthly_price and (yearly_price or auto_generated):
+        if not yearly_price:
+            yearly_price = (monthly_price[0],
+                            _generate_seg_price(monthly_price[1], 12))
         seg.update({"yearly_price": {
             "base_price": yearly_price[0],
             "type": "segmented",
             "segmented": yearly_price[1]
         }})
-    return json.dumps(seg) if seg else None
 
+    return json.dumps(seg) if seg else None
 
 
 if __name__ == '__main__':
@@ -148,124 +168,37 @@ if __name__ == '__main__':
         for name, ram, vcpus, disk in flavors:
             create_flavor(name, ram, vcpus, disk)
 
-    # ip
-    seg_ip_bgp = make_seg_price(
-        ("0", [[100, "0.3611"], [50, "0.2778"], [30, "0.2222"], [0, "0.0300"]]),
-        ("0", [[100, "200"], [50, "150"], [30, "140"], [0, "20"]]),
-        ("0", [[100, "2000"], [50, "1500"], [30, "1400"], [0, "200"]]))
-    seg_ip_tel = make_seg_price(
-        ("0", [[100, "0.3611"], [50, "0.2778"], [30, "0.2222"], [0, "0.0300"]]),
-        ("0", [[100, "200"], [50, "150"], [30, "140"], [0, "20"]]),
-        ("0", [[100, "2000"], [50, "1500"], [30, "1400"], [0, "200"]]))
-
-    # instance
-    instance_micro_1 = make_seg_price(
-        ("0", [[0, "0.0560"]]),
-        ("0", [[0, "40"]]),
-        ("0", [[0, "400"]]))
-    instance_micro_2 = make_seg_price(
-        ("0", [[0, "0.1110"]]),
-        ("0", [[0, "70"]]),
-        ("0", [[0, "700"]]))
-    instance_standard_1 = make_seg_price(
-        ("0", [[0, "0.2220"]]),
-        ("0", [[0, "150"]]),
-        ("0", [[0, "1500"]]))
-    instance_standard_2 = make_seg_price(
-        ("0", [[0, "0.4440"]]),
-        ("0", [[0, "300"]]),
-        ("0", [[0, "3000"]]))
-    instance_standard_4 = make_seg_price(
-        ("0", [[0, "0.8890"]]),
-        ("0", [[0, "600"]]),
-        ("0", [[0, "6000"]]))
-    instance_standard_8 = make_seg_price(
-        ("0", [[0, "1.7780"]]),
-        ("0", [[0, "1200"]]),
-        ("0", [[0, "12000"]]))
-    instance_standard_12 = make_seg_price(
-        ("0", [[0, "2.6670"]]),
-        ("0", [[0, "1900"]]),
-        ("0", [[0, "19000"]]))
-    instance_standard_16 = make_seg_price(
-        ("0", [[0, "3.5560"]]),
-        ("0", [[0, "2500"]]),
-        ("0", [[0, "25000"]]))
-    instance_memory_1 = make_seg_price(
-        ("0", [[0, "0.3610"]]),
-        ("0", [[0, "250"]]),
-        ("0", [[0, "2500"]]))
-    instance_memory_2 = make_seg_price(
-        ("0", [[0, "0.7220"]]),
-        ("0", [[0, "500"]]),
-        ("0", [[0, "5000"]]))
-    instance_memory_4 = make_seg_price(
-        ("0", [[0, "1.4440"]]),
-        ("0", [[0, "1000"]]),
-        ("0", [[0, "10000"]]))
-    instance_memory_8 = make_seg_price(
-        ("0", [[0, "2.8890"]]),
-        ("0", [[0, "2000"]]),
-        ("0", [[0, "20000"]]))
-    instance_memory_12 = make_seg_price(
-        ("0", [[0, "4.3330"]]),
-        ("0", [[0, "3000"]]),
-        ("0", [[0, "30000"]]))
-    instance_compute_2 = make_seg_price(
-        ("0", [[0, "0.3330"]]),
-        ("0", [[0, "200"]]),
-        ("0", [[0, "20000"]]))
-    instance_compute_4 = make_seg_price(
-        ("0", [[0, "0.6670"]]),
-        ("0", [[0, "450"]]),
-        ("0", [[0, "4500"]]))
-    instance_compute_8 = make_seg_price(
-        ("0", [[0, "2.8890"]]),
-        ("0", [[0, "2000"]]),
-        ("0", [[0, "20000"]]))
-    instance_compute_12 = make_seg_price(
-        ("0", [[0, "2.0000"]]),
-        ("0", [[0, "1400"]]),
-        ("0", [[0, "14000"]]))
-
-    # volume
-    volume_size = make_seg_price(
-        ("0", [[0, "0.0020"]]),
-        ("0", [[0, "1.4"]]),
-        ("0", [[0, "14"]]))
-    sata_volume_size = make_seg_price(
-        ("0", [[0, "0.0006"]]),
-        ("0", [[0, "0.4"]]),
-        ("0", [[0, "4"]]))
-
+    seg_ip_bgp = {"hourly_price": ("0", [[100, "0.3611"], [50, "0.2778"], [30, "0.2222"], [0, "0.0300"]])}
     products = [
-        ('instance:micro-1', 'compute', 'test1', '0.0560', instance_micro_1),
-        ('instance:micro-2', 'compute', 'test1', '0.1110', instance_micro_2),
-        ('instance:standard-1', 'compute', 'test1', '0.222', instance_standard_1),
-        ('instance:standard-2', 'compute', 'test1', '0.444', instance_standard_2),
-        ('instance:standard-4', 'compute', 'test1', '0.889', instance_standard_4),
-        ('instance:standard-8', 'compute', 'test1', '1.778', instance_standard_8),
-        ('instance:standard-12', 'compute', 'test1', '2.667', instance_standard_12),
-        ('instance:standard-16', 'compute', 'test1', '3.556', instance_standard_16),
-        ('instance:memory-1', 'compute', 'test1', '0.3610', instance_memory_1),
-        ('instance:memory-2', 'compute', 'test1', '0.7220', instance_memory_2),
-        ('instance:memory-4', 'compute', 'test1', '1.4440', instance_memory_4),
-        ('instance:memory-8', 'compute', 'test1', '2.8890', instance_memory_8),
-        ('instance:memory-12', 'compute', 'test1', '4.3330', instance_memory_12),
-        ('instance:compute-2', 'compute', 'test1', '0.3330', instance_compute_2),
-        ('instance:compute-4', 'compute', 'test1', '0.6670', instance_compute_4),
-        ('instance:compute-8', 'compute', 'test1', '2.8890', instance_compute_8),
-        ('instance:compute-12', 'compute', 'test1', '2.000', instance_compute_12),
-        ('ip.floating.CHINATELECOM', 'network', 'test1', '0.0347', seg_ip_tel),
+        ('instance:micro-1', 'compute', 'test1', '0.0560', None),
+        ('instance:micro-2', 'compute', 'test1', '0.1110', None),
+        ('instance:standard-1', 'compute', 'test1', '0.222', None),
+        ('instance:standard-2', 'compute', 'test1', '0.444', None),
+        ('instance:standard-4', 'compute', 'test1', '0.889', None),
+        ('instance:standard-8', 'compute', 'test1', '1.778', None),
+        ('instance:standard-12', 'compute', 'test1', '2.667', None),
+        ('instance:standard-16', 'compute', 'test1', '3.556', None),
+        ('instance:memory-1', 'compute', 'test1', '0.3610', None),
+        ('instance:memory-2', 'compute', 'test1', '0.7220', None),
+        ('instance:memory-4', 'compute', 'test1', '1.4440', None),
+        ('instance:memory-8', 'compute', 'test1', '2.8890', None),
+        ('instance:memory-12', 'compute', 'test1', '4.3330', None),
+        ('instance:compute-2', 'compute', 'test1', '0.3330', None),
+        ('instance:compute-4', 'compute', 'test1', '0.6670', None),
+        ('instance:compute-8', 'compute', 'test1', '2.8890', None),
+        ('instance:compute-12', 'compute', 'test1', '2.000', None),
         ('ip.floating.BGP', 'network', 'test1', '0.0347', seg_ip_bgp),
-        ('volume.size', 'block_storage', 'test1', '0.0020', volume_size),
-        ('sata.volume.size', 'block_storage', 'test1', '0.0006', sata_volume_size),
+        ('volume.size', 'block_storage', 'test1', '0.0020', None),
+        ('sata.volume.size', 'block_storage', 'test1', '0.0006', None),
         ('snapshot.size', 'block_storage', 'test1', '0.0002', None),
         ('listener', 'network', 'test1', '0.03', None),
         ('alarm', 'monitor', 'test1', '0.03', None),
     ]
     for name, service, region_id, unit_price, extra in products:
         try:
+            if not extra:
+                extra = {"hourly_price": ("0", [[0, unit_price]])}
+            extra = make_seg_price(**extra)
             create_or_update_product(name, service, region_id, unit_price, extra)
         except Exception:
             raise
